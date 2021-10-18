bdba959c582a810e3bca8e8785a3c6552d6f816a
IlarioCOCCO <ilario.cocco@gmail.com>
2021-10-19 00:40:51 +0200

    commi esercizio bad Words

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
-//inizializzo le variabili
-$firstName = $_GET['nome'];
-$age = $_GET['eta'];
-$frase = "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    Provident ad molestias distinctio, 
-    vel recusandae modi repellat."
+// //inizializzo le variabili
+// $firstName = $_GET['nome'];
+// $age = $_GET['eta'];
+// $frase = "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+//     Provident ad molestias distinctio, 
+//     vel recusandae modi repellat.";
+
+// creo variabile a mia scelta con paragrafo di testo  
+$frasebadWords = "avete la faccia come il ...";
+// una parola da censurare viene passata dall' utente tramite GET  
+$badWord = $_GET['badWord'];
+
+
 ?>
 
 
@@ -19,7 +26,7 @@ $frase = "Lorem ipsum dolor sit amet consectetur adipisicing elit.
 </head>
 
 <body>
-    <h1>ciao sono <?php echo $firstName; ?> e ho <?php echo $age; ?> anni</h1>
+    <!-- <h1>ciao sono <?php echo $firstName; ?> e ho <?php echo $age; ?> anni</h1>
     <hr>
     <h2>test echo: <?php echo "hola" ?></h2>
     <p>
@@ -32,7 +39,27 @@ $frase = "Lorem ipsum dolor sit amet consectetur adipisicing elit.
     <hr>
     <p>
         <strong>Posizione parola Amet modificata con parola BreakPoint: </strong><?php echo str_replace('amet', '<strong>breakPoint</strong>', $frase); ?>
+    </p> -->
+
+    <hr>
+    <h1>Esercizio badWords</h1>
+    <hr>
+
+    <p>
+        <strong>frase: </strong> <?php echo $frasebadWords; ?> <strong>lunghezza frase: </strong> <?php echo strlen($frasebadWords) ?> <strong>caratteri</strong>
     </p>
+    <p>
+        <strong>frase: </strong> <?php echo $frasebadWords; ?> <strong>Parola da censurare: </strong><?php echo $badWord; ?>
+    </p>
+    <p>
+        <?php echo $frasebadWords; ?> <?php echo $badWord; ?>
+    </p>
+    <p>
+        <?php echo str_replace('Culo', '(***)', $frasebadWords);  ?>
+    </p>
+
+
+
 </body>
 
 </html>
